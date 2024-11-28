f14b7d439068d5b01fd75dbbf8283beb53d87877
Thrisha020 <thrisha@genairesonance.com>
2024-11-28 11:58:12 +0530

    SEND-DATAONLY changed

--- cobol/EPSCMORT.cbl
+++ cobol/EPSCMORT.cbl
@@ -19,7 +19,7 @@
        01  W-FLAGS.
            10  W-SEND-FLAG                    PIC X.
                88  SEND-ERASE                   VALUE '2'.
-               88  SEND-DATAONLY                VALUE '4'.
+               88  SEND-DATAONLY                VALUE '6'.
                88  SEND-MAPONLY                 VALUE '8'.
                88  SEND-DATAONLY-ALARM          VALUE '10'.
                88  SEND-ALL                     VALUE '10'.
