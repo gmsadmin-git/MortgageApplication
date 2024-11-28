faa56fb9ff0b7058845f92d9031a5b0337393a63
Thrisha020 <thrisha@genairesonance.com>
2024-11-28 11:24:27 +0530

    SEND-DATAONLY-ALARM value is changed

--- cobol/EPSCMORT.cbl
+++ cobol/EPSCMORT.cbl
@@ -21,7 +21,7 @@
                88  SEND-ERASE                   VALUE '1'.
                88  SEND-DATAONLY                VALUE '2'.
                88  SEND-MAPONLY                 VALUE '3'.
-               88  SEND-DATAONLY-ALARM          VALUE '4'.
+               88  SEND-DATAONLY-ALARM          VALUE '5'.
                88  SEND-ALL                     VALUE '10'.
 
        01 W-CONVERSIONS.
