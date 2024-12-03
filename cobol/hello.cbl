212211a913445cbdf7de618e510bb0f354771052
Thrisha020 <thrisha@genairesonance.com>
2024-12-03 14:39:53 +0530

    PROCEDURE DIVISION changed

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'Hai trisha using Kobee Build'.
+           DISPLAY 'Haii trisha using Kobee Build'.
            STOP RUN.
