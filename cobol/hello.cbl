c6b1ea15a1637d1e62902e48af7e1f9bfb3aa802
Leo-hari-messiah <harsih@genairesonance.com>
2024-12-03 13:58:37 +0530

    add spaces

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -1,7 +1,7 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
        ENVIRONMENT DIVISION.
-      * 
+      *
        PROCEDURE DIVISION.
            DISPLAY 'HEYYY trisha using Kobee Build'.
            STOP RUN.
