218d91e6161403fa0fcf05c86b1bceb359243130
gmsadmin-git <tech@graytitude.com>
2024-12-06 20:47:55 +0530

    testing jenkins

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'Hello Trisha Welcome to GMS'.
+           DISPLAY 'Hello Trisha Welcome to GMS-testing jenkins'.
            STOP RUN.
