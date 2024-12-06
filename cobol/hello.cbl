df67afed611fad04aa003e93b521e8d501ebfdcb
ratnamGT <ratnam.gupta@graytitude.com>
2024-12-06 18:46:22 +0530

    testing new jenkins

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'testing Jenkins chatbot feature/Test-demo1'.
+           DISPLAY 'testing Jenkins chatbot feature/Test-demo2'.
            STOP RUN.
