a19ef1b3bf583a66d6718e9e5771ca178ee46ccf
gmsadmin-git <tech@graytitude.com>
2024-12-07 10:46:29 +0530

    Update hello.cbl

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'testing Jenkins chatbot feature/Test-demo2'.
+           DISPLAY 'testing Jenkins chatbot feature/TestDemo jenkins'.
            STOP RUN.
