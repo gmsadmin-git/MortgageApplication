9e08b1db5c26c6fa0a3e68bb94c81d17ffb0cb01
gmsadmin-git <tech@graytitude.com>
2024-12-08 16:29:36 +0530

    Update hello.cbl

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,5 +3,5 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'testing impactbuild testing1'.
+           DISPLAY 'testing jenkins testing'.
            STOP RUN.
