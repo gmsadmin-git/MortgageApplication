8128cb2449550fe961af91c3b80df6891e48d9a4
gmsadmin-git <tech@graytitude.com>
2024-12-12 12:11:41 +0530

    Update heloword.cbl

--- cobol/heloword.cbl
+++ cobol/heloword.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELOWORD.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'Hello Dhanesh'.           
+           DISPLAY 'Hello Dhanesh1'.           
            STOP RUN.
