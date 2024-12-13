0ea0414d5d0336a9befebe01e69c1a88ee90cfb5
gmsadmin-git <tech@graytitude.com>
2024-12-13 20:36:13 +0530

    Update COBDDB2.cbl- testing jenkins from chatbot

--- cobol/COBDDB2.cbl
+++ cobol/COBDDB2.cbl
@@ -4,7 +4,7 @@
          WORKING-STORAGE SECTION.
 
          01  WS-SQLREAD           PIC X(1).
-      *** IKAN DEMO with DBB with IKAN Phases *** 
+      testing jenkins from chatbot *** 
       *** INCLUDING SQLCA COPYBOOK ******
            EXEC SQL
                 INCLUDE SQLCA
