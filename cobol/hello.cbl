101cbd0a2558a38dac1655caf2a46d97e2319fcb
BhavadharaniShankar123 <bhavadharani@genairesonance.com>
2025-01-11 10:38:40 +0530

    janjan

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,7 +3,7 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'Bhava Testing the commit'.
+           DISPLAY 'Bhava Testing the commit janjan'.
            STOP RUN.
            
           
