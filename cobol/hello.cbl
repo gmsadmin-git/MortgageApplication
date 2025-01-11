d7fe52409f1dc809167756b34c76b2224ac30b25
BhavadharaniShankar123 <bhavadharani@genairesonance.com>
2025-01-11 12:09:28 +0530

    frontend_testing

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -3,7 +3,7 @@
        ENVIRONMENT DIVISION.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'Bhava Testing the commit janjan11'.
+           DISPLAY 'frontend_testing_jan11'.
            STOP RUN.
            
           
