c254a2c81fee9c66fc705962fa72b109323a042c
ratnamGT <ratnam.gupta@graytitude.com>
2025-02-05 20:05:08 +0530

    test

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -4,11 +4,9 @@
       *
        PROCEDURE DIVISION.
 
-<<<<<<< HEAD
            DISPLAY 'testing_mainframe_explaining'.
-=======
            DISPLAY 'testing_mainframe23'.
->>>>>>> branch 'main' of https://github.com/gmsadmin-git/MortgageApplication.git
+
 
            STOP RUN.
 
