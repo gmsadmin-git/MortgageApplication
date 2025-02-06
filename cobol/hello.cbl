b3febd225a319945db5c99c729523409525a5e7c
gmsadmin-git <tech@graytitude.com>
2025-02-06 20:25:13 +0530

    Update hello.cbl testainboat

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -5,7 +5,7 @@
        PROCEDURE DIVISION.
 
            DISPLAY 'testing_mainframe'.
-           DISPLAY 'testing_mainframe235'.
+           DISPLAY 'testing_mainframe testaiboat'.
 
 
            STOP RUN.
