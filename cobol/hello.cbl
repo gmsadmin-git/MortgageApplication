d3bb97959911095a9f19895371cf218612c4d168
gmsadmin-git <tech@graytitude.com>
2025-02-08 10:50:17 +0530

    Update hello.cbltestaibot

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -4,7 +4,7 @@
       *
        PROCEDURE DIVISION.
 
-           DISPLAY 'testing_mainframe-testaiboat by ratnam'.
+           DISPLAY 'testing_mainframe-testaiboat'.
            
            STOP RUN.
 
