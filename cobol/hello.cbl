ae98a3d725223daaa0c79b3b8d0f2478a1ac30da
Thrisha020 <thrisha@genairesonance.com>
2025-02-08 17:27:42 +0530

    hu

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -4,7 +4,7 @@
       *
        PROCEDURE DIVISION.
 
-           DISPLAY 'testing_mainframe6byhfurffdvgnrj'.
+           DISPLAY 'testing_mainframe6byhfurffdvggvnrj'.
            DISPLAY 'testing_mainframe1'.
 
 
