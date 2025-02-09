8c805fd8fdbde1f9f19b2d583312a7c34b30c845
gmsadmin-git <tech@graytitude.com>
2025-02-09 14:23:26 +0530

    Update epscmort.cbl

--- cobol/epscmort.cbl
+++ cobol/epscmort.cbl
@@ -73,9 +73,9 @@
        COPY EPSMTCOM.
 
        PROCEDURE DIVISION USING DFHCOMMAREA.
-           DISPLAY "Company: " COMPANY-NAME.
-           DISPLAY "Founded: " YEAR-FOUNDED.
-           DISPLAY WELCOME-MSG.
+      *     DISPLAY "Company: " COMPANY-NAME.
+      *     DISPLAY "Founded: " YEAR-FOUNDED.
+      *     DISPLAY WELCOME-MSG.
 
        EPSCMORT-MAINLINE.
       * Call procedure to do SQL call
