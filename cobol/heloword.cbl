74ab1c0db922bfc8d8b736aedf179154663b0f3b
gmsadmin-git <tech@graytitude.com>
2025-02-11 17:25:19 +0530

    Update heloword.cbltestinmutiple

--- cobol/heloword.cbl
+++ cobol/heloword.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELOWORD.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'Hello Paul from VMImage test 3'.           
+           DISPLAY 'testing multiplefiles'.           
            STOP RUN.
