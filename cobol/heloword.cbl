eca2b2c403af877eadb7a7ea43b6367e922208df
gmsadmin-git <tech@graytitude.com>
2025-02-11 18:30:35 +0530

    Update heloword.cbltestingratnam

--- cobol/heloword.cbl
+++ cobol/heloword.cbl
@@ -2,5 +2,5 @@
        PROGRAM-ID. HELOWORD.
        ENVIRONMENT DIVISION.
        PROCEDURE DIVISION.
-           DISPLAY 'testing multiplefilesagain'.           
+           DISPLAY 'testing ratnam'.           
            STOP RUN.
