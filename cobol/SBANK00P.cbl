fd0ee07906f9b2f59bdaff1b684a23d9bfe659f2
Nandha <nandha@graytitude.com>
2025-02-11 17:41:44 +0530

    Create SBANK00P.cbl

---
+++ cobol/SBANK00P.cbl
@@ -0,0 +1,23 @@
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBDDB2.
+       DATA DIVISION.
+         WORKING-STORAGE SECTION.
+
+         01  WS-SQLREAD           PIC X(1).
+      *** IKAN DEMO with DBB with AIBOT Phase *** 
+      *** INCLUDING SQLCA COPYBOOK ******
+           EXEC SQL
+                INCLUDE SQLCA
+           END-EXEC.
+
+       PROCEDURE DIVISION.
+       MAIN-PARA.
+
+           EXEC SQL
+                SELECT IBMREQD INTO: WS-SQLREAD
+                FROM SYSIBM.SYSDUMMY1
+           END-EXEC.
+           Display 'Testing the cobDdb2'.
+
+           STOP RUN.
