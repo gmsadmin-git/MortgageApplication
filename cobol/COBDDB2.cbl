8292bc4f7454839982f96d35a96518ad1e728695
gmsadmin-git <tech@graytitude.com>
2025-02-14 14:07:41 +0530

    Update COBDDB2.cbl

--- cobol/COBDDB2.cbl
+++ cobol/COBDDB2.cbl
@@ -5,7 +5,7 @@
 
          01  WS-SQLREAD           PIC X(1).
 
-      *** INCLUDING SQLCA test ******
+      *** INCLUDING SQLCA changefile ******
            EXEC SQL
                 INCLUDE SQLCA
            END-EXEC.
