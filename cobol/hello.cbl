90fe84b4fc5f1c940a7bcb59e2e7a7a9d596b670
Nandha-49 <nandha@graytitude.com>
2025-02-19 01:13:56 +0530

    Updated hello.cbl file

--- cobol/hello.cbl
+++
@@ -1,7 +0,0 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. HELLOo.
-       ENVIRONMENT DIVISION.
-      *
-       PROCEDURE DIVISION.
-           DISPLAY 'buildlingg'.
-           STOP RUN.
