2737dc7da289b1ca94a61abbb6c8b6fb0e6baa59
Thrisha020 <thrisha@genairesonance.com>
2025-03-27 11:00:52 +0530

    changedd

--- cobol/hello.cbl
+++ cobol/hello.cbl
@@ -5,7 +5,7 @@
        PROCEDURE DIVISION.
 
            DISPLAY 'testing_mainframe'.
-           DISPLAY 'testing_mainframe12345'.
+           DISPLAY 'testing_mainframe12345 trisha'.
 
 
            STOP RUN.
